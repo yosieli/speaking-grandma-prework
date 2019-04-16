e32f5a104a4717fa3be8603f57c6374a87a3e0dd
feven <feventsegay6@gmail.com>
2019-04-16 13:59:39 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,15 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma
+gretting='I LOVE YOU GRANDMA!'
+phrase='WHAT DID YOU EAT TODAY?'
+if gretting=='I LOVE YOU GRANDMA!'
+  puts 'I LOVE YOU TOO PUMPKIN!'
+  elsif gretting=='Hi Nana, how are you?'
+  puts'HUH?! SPEAK UP, SONNY!'
+  elsif gretting=='Hi!'
+  puts'HUH?! SPEAK UP, SONNY!'
+  elsif phrase=='WHAT DID YOU EAT TODAY?' || phrase=='WHAT?'
+  puts 'NO, NOT SINCE 1938!'
+  end
